158c69b96f8e3a4cab34661dde3802b1ac09b02b
hyecozy <xoonue@gmail.com>
2023-01-04 19:45:04 +0900

    first commit

---
+++ src/test/java/com/isMorP/isMorP/IsMorPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isMorP.isMorP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsMorPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
